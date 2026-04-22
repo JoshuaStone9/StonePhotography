dd9160457c216e29596d1d06f01983db6ec95787
JoshuaStone9 <95473823+JoshuaStone9@users.noreply.github.com>
2026-04-23 00:40:20 +0100

    fix: removed two more inconsistencies and renamed id to be more appropriate

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse navbar-collapse" id="navbar">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <a className="nav-link button" href="#home">Home</a>
